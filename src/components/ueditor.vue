bf49f3aa525ed7aaa49149db33b40a02abf8bde3
tomoyuen <wisyue@yahoo.com>
2016-08-23 16:17:00 +0800

    edit ueditor width;

--- src/components/ueditor.vue
+++ src/components/ueditor.vue
@@ -1,16 +1,32 @@
 <template>
-  <textarea name="textarea" :id="id" rows="8" class="textarea"></textarea>
+  <textarea :name="name" :id="id" class="textarea"></textarea>
 </template>
+
+<style>
+  .textarea {
+    width: 998px;
+  }
+</style>
 
 <script>
   /* global UE */
   export default {
     props: {
       id: String,
-      rows: String,
+      rows: {
+        type: Number,
+        default: 8,
+      },
+      name: String,
     },
     ready() {
-      UE.getEditor(this.id);
+      UE.getEditor(this.id, {
+        toolbars: [
+          ['fullscreen', 'source', 'undo', 'redo', '|', 'bold', 'italic', 'underline', 'strikethrough', '|', 'justifyleft', 'justifycenter', 'justifyright', '|', 'insertorderedlist', 'insertunorderedlist', '|', 'link', 'unlink', '|', 'simpleupload', 'insertimage', 'insertvideo', 'map', 'wordimage', '|', 'horizontal', 'spechars', 'removeformat', '|', 'searchreplace', '|', 'preview'],
+        ],
+        autoHeightEnabled: true,
+        initialFrameHeight: 180,
+      });
     },
   };
 </script>
